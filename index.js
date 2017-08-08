8bd94b09414626634593d4e1d5a294e2b7b6cc9a
Tomasz Sikora <frakti7@gmail.com>
2017-08-08 18:29:39 +0200

    Pipe logs though simple proxy

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-process.stdin.pipe(process.stdout);
+const through = require('through2')
+
+process.stdin.pipe(
+  through(function (chunk, encoding, callback) {
+    console.info('-----')
+    this.push(chunk)
+    callback()
+  }
+)).pipe(process.stdout)
