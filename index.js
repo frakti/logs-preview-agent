61bc518724948d9f6a5b8d13990147a153fe15b6
Tomasz Sikora <frakti7@gmail.com>
2017-08-08 18:35:46 +0200

    Split each log line to separate chunk

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const through = require('through2')
+const split = require('split')
 
-process.stdin.pipe(
+process.stdin.pipe(split()).pipe(
   through(function (chunk, encoding, callback) {
     console.info('-----')
     this.push(chunk)
