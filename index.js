cca5954fb67750bfd8619acafa3862d64aa29119
Tomasz Sikora <frakti7@gmail.com>
2017-08-09 20:51:03 +0200

    Send each log line to backend

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const through = require('through2')
 const split = require('split')
+const fetch = require('node-fetch')
 
 process.stdin.pipe(split()).pipe(
-  through(function (chunk, encoding, callback) {
-    console.info('-----')
-    this.push(chunk)
+  through(function (log, encoding, callback) {
+    fetch('http://localhost:6543/log', {
+      method: 'POST',
+      body: log,
+      headers: {
+        'content-type': 'text/plain'
+      }
+    })
+
+    this.push(log)
+    this.push('\n')
     callback()
   }
 )).pipe(process.stdout)
